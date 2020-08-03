48daf67d97c14009d4b285e5774fdd929e4d9f8c
moosethecoder <mark@markamations.com>
2020-08-03 02:44:53 +0100

    First commit - cat

---
+++ cat/cat.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const AllowedArgs = {
+	'-E':{func:'ShowEnds'},
+	'--show-ends':{func:'ShowEnds'},
+	'-n':{func:'NumberLines'},
+};
+const CommandDesc = {
+	'SimpleNix4Win - Cat':'A cut down version of of the unix cat command, built for windows',
+	'-E, --show-ends':'display $ at end of each line'
+}
+const ArgFunctions = {
+	'ShowEnds':async (str)=>{
+		return new Promise(async resolve => {
+			str = str.split('\r\n');
+			let l = str.length;
+			while(l--){
+				str[l] = str[l]+'$';
+			}
+			str = str.join('\r\n');
+			resolve(str);
+		});
+	},
+	'NumberLines':async (str)=>{
+		return new Promise(async resolve => {
+			str = str.split('\r\n');
+			let l = str.length;
+			while(l--){
+				str[l] = (l + 1)+':'+str[l];
+			}
+			str = str.join('\r\n');
+			resolve(str);
+		})
+	}
+}
+const VERSION = "0.0.1";
+module.exports = async function(UserArgs){
+	// Check the the passed args with allowed args
+	let FileContents = '';
+	let Ammendments = [];
+	for(let argIndex in UserArgs){
+		let arg = UserArgs[argIndex];
+		if(arg === "--help"){
+			for(var HelpIndex in CommandDesc){
+				console.log(HelpIndex+' '+CommandDesc[HelpIndex]);
+			}
+			process.exit();
+		}
+		if(arg === "--version"){
+			console.log(VERSION);
+			process.exit();
+		}
+		if(AllowedArgs[arg]){
+			// This is a valid argument
+			Ammendments.push(ArgFunctions[AllowedArgs[arg].func]);
+		}else{
+			// This is not an arg, is it a file?
+			let isFile = await fs.existsSync(arg);
+			// TODO : Fail on permissions
+			if(isFile){
+				// Yes
+				FileContents = await fs.readFileSync(arg).toString();
+			}else{
+				// No
+				console.log('File "'+arg+'" not found.');
+				process.exit(1);
+			}
+		}
+	}
+	// Run all ammendments
+	for(let AmmendmentIndex in Ammendments){
+		var Ammendment = Ammendments[AmmendmentIndex];
+		FileContents = await Ammendment(FileContents);
+	}
+	console.log(FileContents);
+}
